7dc9b2902b2e4e8f3aaad01a5d8906ba5be292a0
Auri Vincenzi <aurimrv@yahoo.com.br>
2023-03-02 23:53:38 -0300

    feat: Prepare training1 example

--- training1/training1.py
+++ training1/training1.py
@@ -17,17 +17,3 @@ class Training:
          fact = None
 
       return fact
-
-def main():
-   print("Type an integer number: ", end="")
-   n = int(input())
-
-   fact = Training.factorial(n)
-
-   if (fact is None):
-      print("Error: there is no factorial for negative numbers")
-   else:
-      print("Factorial of %d is %d." % (n, fact))
-
-if (__name__ == '__main__'):
-   main()
